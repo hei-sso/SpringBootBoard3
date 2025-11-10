8b0b8944e93a228da4bd54980f6e150bad1b9b33
hei-sso <lak78998@gmail.com>
2025-11-10 16:02:41 +0900

    Markdown

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.mysite.sbb.DataNotFoundException;
+import com.mysite.sbb.user.SiteUser;
 
 import lombok.RequiredArgsConstructor;
 
@@ -34,11 +35,12 @@ public class QuestionService {
 		}
 	}
 	
-    public void create(String subject, String content) {
+    public void create(String subject, String content, SiteUser user) {
         Question q = new Question();
         q.setSubject(subject);
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
+        q.setAuthor(user);
         this.questionRepository.save(q);
     }
     
@@ -47,5 +49,21 @@ public class QuestionService {
     	sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
         return this.questionRepository.findAll(pageable);
+    }
+    
+    public void modify(Question question, String subject, String content) {
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setModifyDate(LocalDateTime.now());
+        this.questionRepository.save(question);
+    }
+    
+    public void delete(Question question) {
+    	this.questionRepository.delete(question);
+    }
+    
+    public void vote(Question question, SiteUser siteUser) {
+    	question.getVoter().add(siteUser);
+    	this.questionRepository.save(question);
     }
 }
